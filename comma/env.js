d48f5b8db2243405e48abb3ced475417fd8eab40
Yonatan Mizrahi <yonatanmiz963@gmail.com>
2021-05-15 11:45:26 +0300

    finalizing env

--- comma/env.js
+++ comma/env.js
@@ -7,33 +7,12 @@ function getEnv(varName, defaultValue=null) {
   return envVariable || defaultValue;
 }
 
-/**
- * The current environment
- */
 const USERNAME = getEnv('DB_USERNAME');
+
 const PASS = getEnv('DB_PASSWORD');
+
 const DB_NAME = getEnv('DB_NAME');
 
-
-/**
- * Linrest exposed port
- */
-const PORT = getEnv('PORT');
-
-/**
- * time in seconds to set for the HTTP server keep-alive timeout (idle connection timeout)
- */
-const KEEP_ALIVE_SEC_TIMEOUT = parseInt(getEnv('KEEP_ALIVE_SEC_TIMEOUT', '182'));
-
-/**
- * How much repository can be selected during assessment
- */
-const ASSESS_LIMIT = getEnv('ASSESS_LIMIT');
-
-/**
- * Token used to encrypt providers token
- */
-const LINB_TOKEN_SECRET = getEnv('LINB_TOKEN_SECRET');
 
 
 module.exports = {
--- server.js
+++ server.js
@@ -6,7 +6,6 @@ require('dotenv').config({path:'./.env'})
 const app = express()
 const http = require('http').createServer(app)
 
-// Express App Config
 app.use(express.json())
 
 if (process.env.NODE_ENV === 'production') {
@@ -21,13 +20,6 @@ if (process.env.NODE_ENV === 'production') {
 
 const todoRoutes = require('./api/todo/todo.routes')
 app.use('/api/todo', todoRoutes)
-
-// Make every server-side-route to match the index.html
-// so when requesting http://localhost:3030/index.html/car/123 it will still respond with
-// our SPA (single page app) (the index.html file) and allow vue/react-router to take it from there
-app.get('/**', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'))
-})
 
 const port = process.env.PORT || 3030
 http.listen(port, () => {
